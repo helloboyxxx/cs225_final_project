d5c499dd3f4527770c1ec6f682f722baea044c05
helloboyxxx <helloboy3x@gmail.com>
2022-11-11 23:38:02 +0000

    unfinished version 2

--- src/graph.h
+++ src/graph.h
@@ -5,6 +5,11 @@
 #include <unordered_map>
 #include "structures.h"
 
+using std::string;
+using std::vector;
+using std::cout;
+using std::endl;
+
 
 class Graph {
   public:
@@ -13,7 +18,7 @@ class Graph {
     Graph();
 
     // Given two file names, this constructor calls graph_generation to help generate the airport_map
-    Graph(std::string airport_filename, std::string route_filename);
+    Graph(string airport_filename, string route_filename);
 
 
     /**
@@ -23,32 +28,45 @@ class Graph {
     * If the given IATA is not in the graph, we return an empty vector.
     * Assume that this is a connected graph.
     */
-    std::vector<std::string> getAdjacentAirports(std::string IATA) const;
+    vector<string> getAdjacentAirports(string& IATA) const;
 
     /*
     Return all the airports in this graph as a vector of their IATAs.
     */
-    std::vector<std::string> getAllAiports() const;
+    vector<string> getAllAiports() const;
     
 
-    /*
+    /**
     Given source and destination airports, if the two airports are directly connected, return the distance
     Otherwise, return -1.
     @param source, the IATA of the source airport
     @param dest, the IATA of the destination airport
-    @return the distance between two airports
+    @returns the distance between two airports.
     */
-    double getDistance(std::string source, std::string dest) const;
-
-
+    double getDistance(string& source, string& dest) const;
 
 
 
   private:
-  /*
-  key: airport's IATA, value: its corresponding airport struct
-  */
-    std::unordered_map<std::string, airport> airport_map;
+    /*
+    key: airport's IATA, value: its corresponding airport struct
+    */
+    std::unordered_map<string, airport> airport_map;
 
+    /**
+     * Returns whether thee route from source to dest exists in the graph
+     * @param source - one airport
+     * @param dest - another airport
+     * @param functionName - the name of the calling function to return
+     */
+    bool assertRouteExists(string& source, string& dest, string functionName) const;
+
+
+    /**
+     * Returns whether a given airport exists in the graph
+     * @param IATA - IATA of an airport
+     * @param functionName - the name of the calling function to return
+     */
+    bool assertAirportExists(string& IATA, string functionName) const;
 
 };
