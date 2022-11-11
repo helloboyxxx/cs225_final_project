54ee3cf5ec4d401fee9a5c8a00a97895e35f5582
helloboyxxx <helloboy3x@gmail.com>
2022-11-11 23:47:04 +0000

    Graph naively done

--- src/graph.cpp
+++ src/graph.cpp
@@ -14,6 +14,8 @@ vector<string> Graph::getAllAiports() const {
 }
 
 vector<string> Graph::getAdjacentAirports(string& IATA) const {
+  assertAirportExists(IATA, __func__);
+
   const airport& A = airport_map.find(IATA)->second;    // A is the Airport struct of the given source
 
   vector<string> airports;
@@ -25,32 +27,34 @@ vector<string> Graph::getAdjacentAirports(string& IATA) const {
 
 
 double Graph::getDistance(string& source, string& dest) const {
-  // I apologize for low readability. I don't want to create too much temporary variables
+  // I apologize for low readability. I don't want to create too much temporary variables.
+  // Use these three lines when debugging. (Since it is easier to understand.)
   // In the end this function should not use assertRouteExists since that will cause to 
   // search for source_a twice. I included the optimized version below. Uncomment to run. 
   assertRouteExists(source, dest, __func__);
   const airport& source_a =  airport_map.find(source)->second;  // source_a is the Airport struct of the given source
   return source_a.adjacent_airport.find(dest)->second.distance; // find the adjacent airport's route and return distance from it
 
-  // Optimized version bellow:
-  const auto& source_it = airport_map.find(source);   // iterator that points to the source in airport_map
-  // Check if the given sources exists
-  if (source_it == airport_map.end()) {
-    cout << "The airport " << source << " is not included in the graph." << endl;
-    return -1;
-  } else if (airport_map.find(dest) == airport_map.end()) {
-    cout << "The airport " << dest << " is not included in the graph." << endl;
-    return -1;
-  }
+  // // Optimized version bellow:
+  // const auto& source_it = airport_map.find(source);   // iterator that points to the source in airport_map
+  // // Check if the given sources exists
+  // if (source_it == airport_map.end()) {
+  //   cout << "The airport " << source << " is not included in the graph." << endl;
+  //   return -1;
+  // } else if (airport_map.find(dest) == airport_map.end()) {
+  //   cout << "The airport " << dest << " is not included in the graph." << endl;
+  //   return -1;
+  // }
 
-  // iterator that points to the dest in the adjacent_airport. Reminder: value is a route struct
-  const auto& dest_it = source_it->second.adjacent_airport.find(dest); 
-  if (dest_it == source_it->second.adjacent_airport.end()) {
-    cout << "The route from " << source << " to " << dest << " is not included in the graph.";
-    return -1;
-  } 
+  // // iterator that points to the dest in the adjacent_airport. Reminder: value is a route struct
+  // const auto& dest_it = source_it->second.adjacent_airport.find(dest); 
+  // if (dest_it == source_it->second.adjacent_airport.end()) {
+  //   cout << "The route from " << source << " to " << dest << " is not included in the graph.";
+  //   return -1;
+  // } 
 
-
+  // // Here we must have a route from source to dest
+  // return dest_it->second.distance;
 }
 
 
@@ -64,6 +68,7 @@ bool Graph::assertRouteExists(string& source, string& dest, string functionName)
   const airport& source_a = airport_map.find(source)->second;   // source_a is the Airport struct of the given source
   if (source_a.adjacent_airport.find(dest) == source_a.adjacent_airport.end()) {
     cout << "The route from " << source << " to " << dest << " is not included in the graph.";
+    cout << " This is called by " << functionName << endl;
     return false;
   }
   return true;
