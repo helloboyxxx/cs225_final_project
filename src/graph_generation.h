108cc13d1b9ced2ea7d9c2825f0cb50fc554cec5
Ruijia Lyu <ruijial3@illinois.edu>
2022-11-13 23:25:32 +0000

    added calculate distance

--- src/graph_generation.h
+++ src/graph_generation.h
@@ -21,3 +21,23 @@ public:
 private:
     static double distance_between(Airport a, Airport b);
 };
+
+/**
+ * Helper function for graph generation. This function calculates 
+ * the distance between to aiports given the latitudes and longitudes of the airports.
+ * This calculation uses the Haversine formula for calculating distance between two points on a sphere.
+ * @param lat1 - latitude of the first airport
+ * @param long1 - longitude of the first airport
+ * @param lat2 - latitude of the second airport
+ * @param long2 - longitude of the second airport
+ * @return the distance between the two airports in kilometers
+*/
+double calculateDistance(double lat1, double long1, double lat2, double long2);
+
+/**
+ * Helper function for calculateDistance function.
+ * This function converts degree into radians.
+ * @param degree - degree that is to be converted
+ * @return the result of convertion
+*/
+double toRadians(const double degree);
