6b1ea907eb5a6b2933bdda579ef8c30738884df2
helloboyxxx <helloboy3x@gmail.com>
2022-11-14 05:03:42 +0000

    Dijkstra partially done

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,17 +1,25 @@
 #include <catch2/catch_test_macros.hpp>
 #include <iostream>
 #include <string>
+#include <functional>
+#include <queue>
+#include <utility>
+#include <vector>
 
 #include "graph.h"
 #include "structures.h"
 
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
 
 TEST_CASE("Empty graph return proper messages") {
   Graph empty_graph;
   std::string source = "ORD";
   std::string dest = "LAS";
   
-  REQUIRE( empty_graph.getAllAiports().empty() );
+  REQUIRE( empty_graph.getAllAirports().empty() );
   REQUIRE(empty_graph.getAdjacentAirports(source).empty());
   REQUIRE(empty_graph.getDistance(source, dest) < 0);
 }
@@ -22,8 +30,8 @@ TEST_CASE("Insert a single airport") {
   // 3830,"Chicago O'Hare International Airport","Chicago","United States","ORD","KORD",41.9786,-87.9048,672,-6,"A","America/Chicago","airport","OurAirports"
   Airport a("ORD", "Chicago", "United States", 41.9786, -87.9048, 672);
   g.insertAirport(a);
-  REQUIRE(g.getAllAiports().size() == 1);
-  REQUIRE(g.getAllAiports().at(0) == "ORD");
+  REQUIRE(g.getAllAirports().size() == 1);
+  REQUIRE(g.getAllAirports().at(0) == "ORD");
   REQUIRE(g.getAdjacentAirports("ORD").empty());
 }
 
@@ -33,7 +41,7 @@ TEST_CASE("Insert and remove one airport") {
   Airport a("ORD", "Chicago", "United States", 41.9786, -87.9048, 672);
   g.insertAirport(a);
   g.removeAirport("ORD");
-  REQUIRE(g.getAllAiports().empty());
+  REQUIRE(g.getAllAirports().empty());
   REQUIRE(g.empty());
 }
 
@@ -55,5 +63,21 @@ TEST_CASE("Insert two airports and one edge") {
   REQUIRE(g.getAdjacentAirports("ORD").size() == 1);
   REQUIRE(g.getAdjacentAirports("ORD").at(0) == "LAS");
   REQUIRE(g.getAdjacentAirports("LAS").empty());
-  REQUIRE(g.getAllAiports().size() == 2);
+  REQUIRE(g.getAllAirports().size() == 2);
+}
+
+typedef std::pair<double, string> disPair;
+TEST_CASE("Test Distance struct") {
+  std::priority_queue<disPair, vector<disPair>, std::greater<disPair>> Q;
+  
+  Q.push({3, "DEN"});
+  Q.push({1, "ORD"});
+  Q.push({2, "LAS"});
+
+  cout << Q.top().second << ": " << Q.top().first << endl;
+  Q.pop();
+  cout << Q.top().second << ": " << Q.top().first << endl;
+  Q.pop();
+  cout << Q.top().second << ": " << Q.top().first << endl;
+  Q.pop();
 }
