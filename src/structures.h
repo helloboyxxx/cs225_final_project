c1745fe5e6d97094eb00fa654e392584fd095fb7
helloboyxxx <helloboy3x@gmail.com>
2022-11-18 23:39:52 +0000

    refined the constructors for both structs

--- src/structures.h
+++ src/structures.h
@@ -4,17 +4,15 @@
 #include <string>
 #include <iostream>
 
+const double InvalidDouble = -114514;
 
 /**
  * This is a struct that holds route information of each airport, including number of flights and distance of travel
 */
 struct Route {
-    Route() {}
-    Route(double distance) {
-        num_flight = 1;
-        this->distance = distance;
-    }
-    unsigned num_flight = 0;
+    Route() : num_flight(0), distance(-1) { }
+    Route(double dist) : num_flight(1), distance(dist) { }
+    unsigned num_flight;
     double distance;
 };
 
@@ -23,16 +21,19 @@ struct Route {
  * It also includes the information about the edge (maybe?).
 */
 struct Airport {
-    // 3-letter IATA code
-    Airport() {}
-    Airport(std::string _IATA, std::string _city, std::string _country, double _lo, double _la, double _al) {
-        IATA = _IATA;
-        city = _city;
-        country = _country;
-        longitude = _lo;
-        latitude = _la;
-        altitude = _al;
-    }
+    /**
+    Default constructor will use initialize this struct with some invalid values:
+    IATA, city, country will be empty string.
+    latitude, longitude, altitude all have special value -114514.
+    route_in, route_out, frequency = 0 by default
+    */
+    Airport()
+        : IATA(""), city(""), country(""), longitude(InvalidDouble),
+          latitude(InvalidDouble), altitude(InvalidDouble), frequency(0) {}
+
+    Airport(std::string _IATA, std::string _city, std::string _country, double _lo, double _la, double _al) : 
+        IATA(_IATA), city(_city), country(_country), longitude(_lo), latitude(_la), altitude(_al), frequency(0) {  }
+    
     std::string IATA;
 
     // the city the airport belongs to
@@ -56,5 +57,5 @@ struct Airport {
     /**
     * Key: IATA of adjacent airports, value: distance of this route
     */
-    std::unordered_map< std::string, Route> adjacent_airport;
+    std::unordered_map<std::string, Route> adjacent_airport;
 };
